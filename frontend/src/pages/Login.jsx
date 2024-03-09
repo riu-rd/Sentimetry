3e265be3dca5c69393ab399aba5e6ca790ee1e21
Alroy-Reyes <106812404+Alroy-Reyes@users.noreply.github.com>
2024-03-10 02:30:23 +0800

    Login Register css update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import '../tailwind.css'; // Import Tailwind CSS file
 
-const Login = ({user}) => {
+const Login = ({ user }) => {
   // Check if user is already authenticated
   const navigate = useNavigate();
   if (user) {
@@ -10,7 +11,7 @@ const Login = ({user}) => {
   }
 
   // Declarations
-  const [credentials, setCredentials] = useState({email: '', password: ''});
+  const [credentials, setCredentials] = useState({ email: '', password: '' });
 
   // When credential values change
   const handleChange = (e) => {
@@ -18,50 +19,56 @@ const Login = ({user}) => {
   }
 
   // When log in button is clicked
-  const handleRegister = (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
 
     const auth = getAuth();
     signInWithEmailAndPassword(auth, credentials.email, credentials.password)
-    .then((userCredential) => {
-      const userEmail = userCredential.user.email;
-      alert(`Successfully Logged in: ${userEmail}`)
-    })
-    .catch((error) => {
-      alert("Error Logging In");
-      console.error(error);
-    });
+      .then((userCredential) => {
+        const userEmail = userCredential.user.email;
+        alert(`Successfully Logged in: ${userEmail}`)
+      })
+      .catch((error) => {
+        alert("Error Logging In");
+        console.error(error);
+      });
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form 
-      onSubmit={handleRegister}>
-        <label htmlFor='email'>Email</label>
-        <input
-          type='email'
-          placeholder='Enter your Email'
-          name='email'
-          value={credentials.email}
-          onChange={handleChange}
-          required
-        />
-
-        <label htmlFor='password'>Password</label>
-        <input
-          type='password'
-          placeholder='Enter your Password'
-          name='password'
-          value={credentials.password}
-          onChange={handleChange}
-          required
-        />
-
-        <button type='submit'>Login</button>
-        
+    <section className="form-container bg-white rounded-lg shadow-lg p-8">
+      <form onSubmit={handleLogin} className="space-y-4">
+        <div>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            placeholder='Enter your Email'
+            name='email'
+            value={credentials.email}
+            onChange={handleChange}
+            required
+            className="form-input rounded-md"
+          />
+        </div>
+        <div>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            placeholder='Enter your Password'
+            name='password'
+            value={credentials.password}
+            onChange={handleChange}
+            required
+            className="form-input rounded-md"
+          />
+          <div className="flex items-center justify-between">
+            <div>
+              <Link to='/register' className="form-link">Don't have an account?</Link>
+            </div>
+            <button type='submit' className="form-button rounded-3xl mt-4 bg-yellow-400 hover:bg-yellow-500">Login</button>
+          </div>
+        </div>
       </form>
-      <Link to='/register'>Go to Register</Link>
+      
     </section>
   )
 }
